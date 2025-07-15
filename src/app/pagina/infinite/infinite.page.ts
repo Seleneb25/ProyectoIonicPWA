397d0574b49a0941752786fd9a0e425b0e984aa5
Seleneb25 <202200033@uprr.edu.mx>
2025-07-14 21:54:36 -0500

    Initial commit

---
+++ src/app/pagina/infinite/infinite.page.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
+
+@Component({
+  selector: 'app-infinite',
+  templateUrl: './infinite.page.html',
+  styleUrls: ['./infinite.page.scss'],
+   imports: [IonicModule, CommonModule, RouterModule, ComponentesModule],
+})
+export class InfinitePage implements OnInit {
+datos=Array(140);
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 1; i < 51; i++) {
+      this.datos.push(`Datos ${i}`);
+    }
+  }
+
+}
